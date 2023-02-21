8f0fb55896f2b87bf4bc7abc46531d075f2becdc
chaen-takumi <chaen.ta1227@gmail.com>
2023-02-21 22:00:24 +0900

    4

--- app/src/main/java/com/example/paint_app/MyView.kt
+++ app/src/main/java/com/example/paint_app/MyView.kt
@@ -10,44 +10,42 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewTreeObserver.OnDrawListener
 
-//1）Viewを継承したクラス
+//１）Viewを継承したクラス
 class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-    private var path:  Path = Path()//線を引く、図形を書く等の描写
-    private var paint: Paint = Paint()//色とか太さ
-    private var drawX: Float = 0F
-    private var drawY: Float = 0F
+    private var path : Path =Path()//線を引く、図形を描く、グラフィック描画
+    private var paint : Paint =Paint()//色とか太さ
+    private var drawX:Float= 0F
+    private var drawY:Float= 0F
 
-
-    //2)onDraw
+    //２）onDraw(描画の準備)
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        paint.color = Color.BLUE
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = 20F
-        canvas?.drawPath(path, paint)
+        paint.color =Color.BLUE//色
+        paint.style=Paint.Style.STROKE//描画のスタイルを線にする
+        paint.strokeWidth = 20F//幅
+        canvas?.drawPath(path,paint)
     }
 
-    //3)実際の描画
+    //３）実際の描画（押した時、動かした時）
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-
-        //タッチポジション
+        //タッチポジション（x座標、y座標）
         drawX = event!!.x
         drawY = event.y
 
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> path.moveTo(drawX, drawY)
-            MotionEvent.ACTION_MOVE -> path.lineTo(drawX, drawY)
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> path.moveTo(drawX,drawY)
+            MotionEvent.ACTION_MOVE -> path.lineTo(drawX,drawY)
         }
-
+        //再描画を実行させる呪文
         invalidate()
 
         //return super.onTouchEvent(event)
         return true
     }
 
-    //4)クリア処理
-    fun clearCanvas() {
+    //４）クリア処理（の関数を作る）
+    fun clearCanvas(){
         path.reset()
         invalidate()
     }
